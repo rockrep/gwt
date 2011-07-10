2eb6a4264cbfaefc6a84fc273f08de60317c7392
John Manoogian III <jm3@jm3.net>
2011-07-10 15:31:07 -0700

    get skeletal tests working with VCR!

--- spec/gwt_spec.rb
+++ spec/gwt_spec.rb
@@ -14,6 +14,9 @@ VCR.config do |c|
   c.stub_with :webmock # or :fakeweb
 end
 
+GWT_API = "https://www.google.com/webmasters/tools/feeds"
+GWT_XML_NS = "http://schemas.google.com/webmasters/tools/2007"
+TEST_SITE = "http%3A%2F%2F140proof.com%2F"
 
 describe "toolkit" do
 
@@ -21,20 +24,75 @@ describe "toolkit" do
     YAML.load_file( "credentials.yml" )
   end
 
-  def sites_feed
+  def get_gwt_feed( uri )
     credentials = creds
-    sites_feed = "https://www.google.com/webmasters/tools/feeds/sites/"
     client = GData::Client::WebmasterTools.new
     client.clientlogin( credentials["user"], credentials["password"] )
-    feed = client.get( sites_feed ).to_xml
+    Nokogiri::XML( client.get( uri ).to_xml.to_s )
+  end
+
+  def crawlissues_feed
+    get_gwt_feed( "#{GWT_API}/#{TEST_SITE}/crawlissues/" )
+  end
+
+  def keywords_feed
+    get_gwt_feed( "#{GWT_API}/#{TEST_SITE}/keywords/" )
+  end
+
+  def messages_feed
+    get_gwt_feed( "#{GWT_API}/messages/" )
+  end
+
+  def sitemaps_feed
+    get_gwt_feed( "#{GWT_API}/#{TEST_SITE}/sitemaps/" )
+  end
+
+  def sites_feed
+    get_gwt_feed( "#{GWT_API}/sites/" )
+  end
+
+  context "crawl issues feed" do
+    use_vcr_cassette "crawlissues_feed"
+    it "should be named 'Crawl Issues'" do
+      crawlissues_feed.css( "feed/title" ).text.should == "Crawl Issues"
+    end
+    it "should contain crawl issues" do
+      crawlissues_feed.css( "entry" ).size.should be > 0
+    end
+  end
+
+  context "keywords feed" do
+    use_vcr_cassette "keywords_feed"
+    it "should be named 'Keywords'" do
+      keywords_feed.css( "title" ).text.should == "Keywords"
+    end
+    it "should contain some keywords" do
+      keywords_feed.xpath( "//wt:keyword", "wt" => GWT_XML_NS).size.should be > 0
+    end
+  end
+
+  context "messages feed" do
+    use_vcr_cassette "messages_feed"
+    it "should be named 'Messages'" do
+      messages_feed.css( "title" ).text.should == "Messages"
+    end
+    it "should contain messages" do
+      pending "i have no messages in my account right now."
+      messages_feed.css( "entry" ).size.should be > 0
+    end
+  end
+
+  context "sitemaps feed" do
+    use_vcr_cassette "sitemaps_feed"
+    it "'s title should be the parent site" do
+      sitemaps_feed.css( "title" ).text.should == CGI.unescape(TEST_SITE )
+    end
   end
 
   context "sites feed" do
     use_vcr_cassette "sites_feed"
     it "should contain entries" do
-      feed =  Nokogiri::XML( sites_feed.to_s )
-      entries = feed.css( "entry" )
-      entries.size.should_not != 0
+      sites_feed.css( "entry" ).size.should be > 0
     end
   end
 
