fa5800b8c3b88c854d80b680cd900373fcddbae8
John Manoogian III <jm3@jm3.net>
2011-07-12 00:58:37 -0700

    many goodness changes in specs:
    * test add and delete
    * add rspec 2 hooks for VCR
    * user VCR "correctly"! :D

--- spec/gwt_spec.rb
+++ spec/gwt_spec.rb
@@ -13,72 +13,101 @@ require "lib/gwt/GwtUser"
 
 VCR.config do |c|
   c.cassette_library_dir = "fixtures/cassettes"
-  c.stub_with :webmock # or :fakeweb
+  c.stub_with :fakeweb
+  c.default_cassette_options = { :record => :new_episodes }
+end
+
+RSpec.configure do |c|
+  c.extend VCR::RSpec::Macros
 end
 
 describe "GWT" do
   TEST_SITE = "http%3A%2F%2F140proof.com%2F"
 
-  context "a new account" do
-    VCR.use_cassette( "blanconiño" ) do
-      u = GwtUser.new( "new_account" )
+  context "new account" do
+    use_vcr_cassette
 
-      use_vcr_cassette "messages_feed"
-      it "should have a messages feed" do
-        u.messages.css( "title" ).text.should == "Messages"
-      end
-
-      it "messages feed should be empty" do
-        u.messages.css( "entry" ).size.should be == 0
-      end
-
-      use_vcr_cassette "sites_feed"
-      it "should contain no sites" do
-        u.sites.css( "entry" ).size.should be == 0
-      end
-
+    before(:each) do
+      @u = GwtUser.new( "new_account" )
     end
+
+    it "should have a messages feed" do
+      @u.messages.css( "title" ).text.should == "Messages"
+    end
+
+    it "messages feed should be empty" do
+      @u.messages.css( "entry" ).size.should be == 0
+    end
+
+    it "should contain no sites" do
+      @u.sites.css( "entry" ).size.should be == 0
+    end
+
   end
 
-  context "an account with sites" do
-    VCR.use_cassette( "oldtimer" ) do
-      u = GwtUser.new( "account_with_sites" )
+  context "account with sites" do
+    use_vcr_cassette
 
-      it "should have a Crawl Issues feed" do
-        u.crawlissues.css( "feed/title" ).text.should == "Crawl Issues"
-      end
-      it "crawl issues feed should contain crawl issues" do
-        u.crawlissues.css( "entry" ).size.should be > 0
-      end
+    before(:each) do
+      @u = GwtUser.new( "account_with_sites" )
+    end
 
-      use_vcr_cassette "keywords"
-      it "should have a keywords feed" do
-       u.keywords.css( "title" ).text.should == "Keywords"
-      end
-      it "keywords feed should contain some keywords" do
-       u.keywords.xpath( "//wt:keyword", "wt" => GwtUser::XML_NS).size.should be > 0
-      end
+    it "should have a Crawl Issues feed" do
+      @u.crawlissues.css( "feed/title" ).text.should == "Crawl Issues"
+    end
 
-      use_vcr_cassette "messages"
-      it "should have a messages feed" do
-        u.messages.css( "title" ).text.should == "Messages"
-      end
-      it "messages feed should contain messages" do
-        pending "how do i make messages appear in my account?"
-        u.messages.css( "entry" ).size.should be > 0
-      end
+    it "crawl issues feed should contain crawl issues" do
+      @u.crawlissues.css( "entry" ).size.should be > 0
+    end
 
-      use_vcr_cassette "sitemaps"
-      it "sitemaps feed should contain the parent site" do
-        u.sitemaps.css( "title" ).text.should == CGI.unescape( TEST_SITE )
-      end
+    it "should have a keywords feed" do
+      @u.keywords.css( "title" ).text.should == "Keywords"
+    end
 
-      use_vcr_cassette "sites"
-      it "sites feed should contain entries" do
-        u.sites.css( "entry" ).size.should be > 0
-      end
+    it "keywords feed should contain some keywords" do
+      @u.keywords.xpath( "//wt:keyword", "wt" => GwtUser::XML_NS).size.should be > 0
+    end
 
-    end # end vcr context
-  end # end rspec context
+    it "should have a messages feed" do
+      @u.messages.css( "title" ).text.should == "Messages"
+    end
 
+    it "messages feed should contain messages" do
+      pending "how do i make messages appear in my account?"
+      @u.messages.css( "entry" ).size.should be > 0
+    end
+
+    it "sitemaps feed should contain the parent site" do
+      @u.sitemaps.css( "title" ).text.should == CGI.unescape( TEST_SITE )
+    end
+
+    it "sites feed should contain entries" do
+      @u.sites.css( "entry" ).size.should be > 0
+    end
+
+  end
+
+  context "add new site" do
+    use_vcr_cassette
+
+    before(:each) do
+      @u = GwtUser.new( "new_account" )
+      @site = "http://onebuttondeploy.com/"
+    end
+
+    it "should succeed" do
+      @post_data = <<-XML
+      <atom:entry xmlns:atom='http://www.w3.org/2005/Atom'>
+        <atom:content src="#{@site}" />
+      </atom:entry>
+      XML
+      @response = @u.add_site( @post_data )
+      @response.status_code.should == 201
+    end
+
+    it "sites feed should contain the new site" do
+      @u.sites.css( "entry title" ).text.should == @site
+    end
+
+  end
 end
